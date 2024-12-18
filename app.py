1813328c055b86abf3f65ebd10deee209da13878
kurch <kurchenko.svetlana90@gmail.com>
2024-12-18 21:46:56 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    result = None
+    error = None
+
+    if request.method == "POST":
+        try:
+            number = float(request.form["number"])
+            result = number ** 3
+            print(f"Результат: {result}")
+        except ValueError:
+            error = "Введите число"
+
+    return render_template("index.html", result=result, error=error)
+
+if __name__ == "__main__":
+    app.run(debug=True)
